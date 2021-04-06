4e2960fe3f9cd9edcc3b2e9ade51346a46bc5ca2
Padrition <padrition@gmail.com>
2021-04-06 16:53:54 +0600

    added head and tail fields to the Snake struct and rewrote the new function. Added functin update what takes the Direction enum and moves every snake part dased on this direction

--- src/snake.rs
+++ src/snake.rs
@@ -3,13 +3,21 @@ use super::*;
 
 pub struct Snake {
     pub sign: char,
-    pub position: Vec<Position>,
+    pub head: Position,
+    pub tail: Vec<Position>,
 }
 impl Snake {
     pub fn new() -> Snake {
         Snake {
             sign: PLAYER_SIGN,
-            position: vec![Position { x: 0, y: 0 }],
+            head: Position { x: 1, y: 0 },
+            tail: vec![Position { x: 0, y: 0 }],
+        }
+    }
+    pub fn update(&mut self, dir: &Direction) {
+        self.head.move_to_dir(dir);
+        for tail_part in self.tail.iter_mut() {
+            tail_part.move_to_dir(dir);
         }
     }
 }
