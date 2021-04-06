b0ac01302af88af4c293e08634fbabe9317e4681
Padrition <padrition@gmail.com>
2021-04-06 22:43:27 +0600

    added function update for updating the board

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
 use super::*;
+use super::snake::*;
 use std::fmt;
 
 #[derive(Clone)]
@@ -25,6 +26,17 @@ impl Board {
         Board {
             board: vec![vec![Cell::Empty; BOARD_SIZE]; BOARD_SIZE],
         }
+    }
+    pub fn update(&mut self, snake : &mut Snake){
+        //reset the board to empty 
+        self.board.fill(vec![Cell::Empty; BOARD_SIZE]);
+        //take a position of snake's head and set cell for Player in the corresponding position in the board 
+        self.board[snake.head.y][snake.head.x] = Cell::Player;
+        //iterate over snake's tail and set cell with a corresponding position to Player 
+        for tail_part in snake.tail.iter(){
+            self.board[tail_part.y][tail_part.x] = Cell::Player;
+        }
+
     }
     pub fn draw(&mut self) {
         print!("{}[2J", 27 as char); //clears the terminal
