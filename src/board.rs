43ca44e43541df846c1071efba5e5e4b29324cae
Padrition <padrition@gmail.com>
2021-04-06 18:50:35 +0600

    added a Cell enum that holds all possible Values of a board cell and implementd Display trait for it.

--- src/board.rs
+++ src/board.rs
@@ -1,19 +1,36 @@
 use super::*;
+use std::fmt;
+
+#[derive(Clone)]
+enum Cell{
+    Empty,
+    Player,
+    Apple
+}
+impl fmt::Display for Cell{
+    fn fmt(&self, f : &mut fmt::Formatter<'_>) -> fmt::Result{
+        match self{
+            Cell::Empty => write!(f, "."),
+            Cell::Player => write!(f, "#"),
+            Cell::Apple => write!(f, "@"),
+        }
+    }
+}
 
 pub struct Board {
-    board: Vec<Vec<char>>,
+    board: Vec<Vec<Cell>>,
 }
 impl Board {
     pub fn new() -> Board {
         Board {
-            board: vec![vec![BOARD_SIGN; BOARD_SIZE]; BOARD_SIZE],
+            board: vec![vec![Cell::Empty; BOARD_SIZE]; BOARD_SIZE],
         }
     }
     pub fn draw(&mut self) {
         print!("{}[2J", 27 as char); //clears the terminal
         for row in self.board.iter() {
-            for sign in row.iter() {
-                print!("{}", sign);
+            for cell in row.iter() {
+                print!("{}", cell);
             }
             println!();
         }
