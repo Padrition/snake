7bcba3684ebafd3b8441e5848b70e1c6dd5710af
Padrition <padrition@gmail.com>
2021-04-06 16:57:56 +0600

    added a function set for the enum Direction that takes either of wasd and converts to a corresponding direction. Added DirectionError so set function can return it as an Err()

--- src/position.rs
+++ src/position.rs
@@ -3,7 +3,7 @@ pub struct Position {
     pub y: usize,
 }
 impl Position {
-    pub fn move_to_dir(&mut self, dir: Direction) {
+    pub fn move_to_dir(&mut self, dir: &Direction) {
         match dir {
             Direction::Up => self.y -= 1,
             Direction::Right => self.x += 1,
@@ -13,9 +13,24 @@ impl Position {
     }
 }
 
+pub enum DirectionError {
+    UnknownDirection,
+}
+
 pub enum Direction {
     Up,
     Right,
     Left,
     Down,
+}
+impl Direction {
+    pub fn set(dir: &str) -> Result<Direction, DirectionError> {
+        match dir.trim() {
+            "w" => Ok(Direction::Up),
+            "d" => Ok(Direction::Right),
+            "a" => Ok(Direction::Left),
+            "s" => Ok(Direction::Down),
+            _ => Err(DirectionError::UnknownDirection),
+        }
+    }
 }
