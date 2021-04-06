b3a268e5eaffab3b13205ab16c770ddd04a751d1
Padrition <padrition@gmail.com>
2021-04-06 16:55:12 +0600

    removed all unrelated functions and structs form board

--- src/board.rs
+++ src/board.rs
@@ -1,15 +1,12 @@
-use super::snake::*;
 use super::*;
 
 pub struct Board {
     board: Vec<Vec<char>>,
-    pub snake: Snake,
 }
 impl Board {
     pub fn new() -> Board {
         Board {
             board: vec![vec![BOARD_SIGN; BOARD_SIZE]; BOARD_SIZE],
-            snake: Snake::new(),
         }
     }
     pub fn draw(&mut self) {
