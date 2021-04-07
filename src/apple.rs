1e4b1826b19bf60866cbc885c8e9a6068eb6c211
Padrition <padrition@gmail.com>
2021-04-07 20:34:12 +0600

    Added field eaten. Rewrote new. Added func update_pos

--- src/apple.rs
+++ src/apple.rs
@@ -3,16 +3,23 @@ use super::*;
 use rand::prelude::*;
 
 pub struct Apple {
+    pub eaten : bool,
     pub pos: Position,
 }
 
 impl Apple {
     pub fn new() -> Apple {
         Apple {
+            eaten : false,
             pos: Position {
                 x: thread_rng().gen_range(0..BOARD_SIZE),
                 y: thread_rng().gen_range(0..BOARD_SIZE),
             },
         }
+    }
+    pub fn update_pos(&mut self){
+        self.eaten = false;
+        self.pos.x = thread_rng().gen_range(0..BOARD_SIZE);
+        self.pos.y = thread_rng().gen_range(0..BOARD_SIZE);
     }
 }
