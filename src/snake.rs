3cd918b6e1e96c1f5593fa66e437735c6bd5a9e9
Padrition <padrition@gmail.com>
2021-04-07 14:52:07 +0600

    renamed snakes update method to next move. Now this method will rewrite snakes tail parts positions to the next part psotion, the first most tail part will get head's position and head will me moved in the provided direction

--- src/snake.rs
+++ src/snake.rs
@@ -14,10 +14,18 @@ impl Snake {
             tail: vec![Position { x: 0, y: 0 }],
         }
     }
-    pub fn update(&mut self, dir: &Direction) {
-        self.head.move_to_dir(dir);
-        for tail_part in self.tail.iter_mut() {
-            tail_part.move_to_dir(dir);
+    pub fn next_move(&mut self, dir: &Direction) {
+        //set first tail part's position to head's position 
+        self.tail[0] = self.head.clone();
+        //increment every tail part to the next position
+        for i in (1..self.tail.len()).rev(){
+            if i == 0{
+                break;
+            }else{
+                self.tail[i] = self.tail[i-1].clone();
+            }
         }
+        //move a head into the direction
+        self.head.move_to_dir(dir);
     }
 }
