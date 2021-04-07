98925d2b713d67ace6a9934ada7cfbdad2706b4c
Padrition <padrition@gmail.com>
2021-04-07 16:06:36 +0600

    changed fmt's match arms to refer to constants

--- src/board.rs
+++ src/board.rs
@@ -1,18 +1,18 @@
-use super::*;
 use super::snake::*;
+use super::*;
 use std::fmt;
 
 #[derive(Clone)]
-enum Cell{
+enum Cell {
     Empty,
     Player,
-    Apple
+    Apple,
 }
-impl fmt::Display for Cell{
-    fn fmt(&self, f : &mut fmt::Formatter<'_>) -> fmt::Result{
-        match self{
-            Cell::Empty => write!(f, "."),
-            Cell::Player => write!(f, "#"),
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Cell::Empty => write!(f, "{}", BOARD_SIGN),
+            Cell::Player => write!(f, "{}", PLAYER_SIGN),
             Cell::Apple => write!(f, "@"),
         }
     }
@@ -27,16 +27,15 @@ impl Board {
             board: vec![vec![Cell::Empty; BOARD_SIZE]; BOARD_SIZE],
         }
     }
-    pub fn update(&mut self, snake : &mut Snake){
-        //reset the board to empty 
+    pub fn update(&mut self, snake: &mut Snake) {
+        //reset the board to empty
         self.board.fill(vec![Cell::Empty; BOARD_SIZE]);
-        //take a position of snake's head and set cell for Player in the corresponding position in the board 
+        //take a position of snake's head and set cell for Player in the corresponding position in the board
         self.board[snake.head.y][snake.head.x] = Cell::Player;
-        //iterate over snake's tail and set cell with a corresponding position to Player 
-        for tail_part in snake.tail.iter(){
+        //iterate over snake's tail and set cell with a corresponding position to Player
+        for tail_part in snake.tail.iter() {
             self.board[tail_part.y][tail_part.x] = Cell::Player;
         }
-
     }
     pub fn draw(&mut self) {
         print!("{}[2J", 27 as char); //clears the terminal
