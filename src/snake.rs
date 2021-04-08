ce23a47e409ab2950e86916225c96a5632a809e3
Padrition <padrition@gmail.com>
2021-04-08 13:10:37 +0600

    Fixed bug with snake's growth being 1 position off

--- src/snake.rs
+++ src/snake.rs
@@ -12,21 +12,21 @@ impl Snake {
         }
     }
     pub fn next_move(&mut self, dir: &Direction) {
-        //set first tail part's position to head's position
-        self.tail[0] = self.head.clone();
         //increment every tail part to the next position
-        for i in (1..self.tail.len()).rev() {
+        for i in (0..self.tail.len()).rev() {
             if i == 0 {
                 break;
             } else {
                 self.tail[i] = self.tail[i - 1].clone();
             }
         }
+        //set first tail part's position to head's position
+        self.tail[0] = self.head.clone();
         //move a head into the direction
         self.head.move_to_dir(dir);
     }
     pub fn grow(&mut self) {
-        let last_index = self.tail.len() - 1;
-        self.tail.push(self.tail[last_index].clone());
+            let last_index = self.tail.len() - 1;
+            self.tail.push(self.tail[last_index].clone());
     }
 }
